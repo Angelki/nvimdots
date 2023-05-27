6c1127156e0fbac5cc64fee9f684da9d15505694
Edward <3zangelks@gmail.com>
2023-05-28 03:20:20 +0400

    format

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,7 +3,7 @@ local home = require("core.global").home
 
 -- Set it to false if you want to use https to update plugins and treesitter parsers.
 ---@type boolean
-settings["use_ssh"] = true
+settings["use_ssh"] = false
 
 -- Set it to false if there are no need to format on save.
 ---@type boolean
@@ -19,9 +19,7 @@ settings["diagnostics_virtual_text"] = true
 
 -- Set the format disabled directories here, files under these dirs won't be formatted on save.
 ---@type string[]
-settings["format_disabled_dirs"] = {
-	home .. "/format_disabled_dir_under_home",
-}
+settings["format_disabled_dirs"] = {home .. "/format_disabled_dir_under_home"}
 
 -- NOTE: The startup time will be slowed down when it's true.
 -- Set it to false if you don't use nvim to open big files.
@@ -58,30 +56,23 @@ settings["external_browser"] = "chrome-cli open"
 -- Filetypes in this list will skip lsp formatting if rhs is true.
 ---@type table<string, boolean>
 settings["formatter_block_list"] = {
-	lua = false, -- example
+    lua = false -- example
 }
 
 -- Servers in this list will skip setting formatting capabilities if rhs is true.
 ---@type table<string, boolean>
 settings["server_formatting_block_list"] = {
-	lua_ls = true,
-	tsserver = true,
-	clangd = true,
-	pylsp = true,
+    lua_ls = true,
+    tsserver = true,
+    clangd = true,
+    pylsp = true
 }
 
 -- Set the language servers that will be installed during bootstrap here.
 -- check the below link for all the supported LSPs:
 -- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/server_configurations
 ---@type string[]
-settings["lsp_deps"] = {
-	"bashls",
-	"clangd",
-	"html",
-	"jsonls",
-	"lua_ls",
-	"pylsp",
-	-- "gopls",
+settings["lsp_deps"] = {"bashls", "clangd", "html", "jsonls", "lua_ls", "pylsp" -- "gopls",
 }
 
 -- Set the general-purpose servers that will be installed during bootstrap here.
@@ -89,14 +80,6 @@ settings["lsp_deps"] = {
 -- in `code_actions`, `completion`, `diagnostics`, `formatting`, `hover` folders:
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
 ---@type string[]
-settings["null_ls_deps"] = {
-	"black",
-	"clang_format",
-	"prettier",
-	"rustfmt",
-	"shfmt",
-	"stylua",
-	"vint",
-}
+settings["null_ls_deps"] = {"black", "clang_format", "prettier", "rustfmt", "shfmt", "stylua", "vint"}
 
 return settings
